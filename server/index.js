2886733dd43469259a62be10b1350edde6a12cdd
ftouuh <zaydftouh79@gmail.com>
2024-03-11 13:31:05 +0000

    conflicts fixed

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { Router } from 'express';
+
+import MessageRoute from './Routes/MessageRoute.js';
+const router = Router()
+import ChatRoute from './Routes/ChatRoute.js';
+
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const db_con_str=process.env.DB_CON_STR;
+
+
+
+app.use(express.json);
+
+app.listen(PORT,(err)=>{
+    if(err){
+        console.log('server is down ');
+    }
+    else{
+        console.log('server is good ');
+    }
+})
+
+
+mongoose.connect(db_con_str).then(()=>{
+    console.log('connected to database');
+
+}).catch(
+    (error)=>{
+        console.log('failed to connect to database');
+    }
+)
+
+app.use("/message", MessageRoute)
+app.use('/chat', ChatRoute);
