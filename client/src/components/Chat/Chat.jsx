6fb49ad8b8530d9077c37b27c27298d601079201
Jihad <jihadbourbab79@gmail.com>
2024-03-12 12:17:53 +0000

    ok

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 export default function Chat() {    
 
-    axios.get("http:/localhost:3000/")
+    axios.get("http:/localhost:3000/chat/UserName")
   return (
     <div>
       
